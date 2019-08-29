d931738747a339f2217821643a823168378c85eb
拉比克 <shenpengfei@bilibili.com>
2019-08-29 17:45:25 +0800

    change px to em/rem

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,14 +4,17 @@ import styled from 'styled-components'
 import CanvasLayer from './CanvasLayer'
 
 const MainContainer = styled.div`
-  padding-top: 24px;
-  max-width: 980px;
+  padding: 2.625rem 1.3125rem;
+  max-width: 48rem;
   margin-left: auto;
   margin-right: auto;
 
-  @media (max-width: 1028px) {
-    margin-left: 24px;
-    margin-right: 24px;
+  @media (max-width: 48rem) {
+    & .code {
+      margin-left: -1.3125rem;
+      margin-right: -1.3125rem;
+      border-radius: 0;
+    }
   }
 `
 
