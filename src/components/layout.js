dc51de83dbe771a75e3c4388ab9f963c70f415ea
拉比克 <shenpengfei@bilibili.com>
2019-08-29 11:23:18 +0800

    page transition time

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Helmet } from 'react-helmet'
 import styled from 'styled-components'
 import CanvasLayer from './CanvasLayer'
@@ -16,16 +16,29 @@ const MainContainer = styled.div`
 `
 
 export default ({ children, location }) => {
+  const [visiable, setVisiable] = useState(false)
+  const path = useRef(location.pathname)
+  useEffect(() => {
+    if (path.current !== location.pathname) {
+      path.current = location.pathname
+      setVisiable(false)
+    }
+    const renderTimeout = setTimeout(() => setVisiable(true), 500)
+    return () => clearTimeout(renderTimeout)
+  }, [location.pathname])
+  const shouldRender =
+    path.current === location.pathname &&
+    (visiable || typeof window === `undefined`)
   return (
     <>
       <Helmet>
         <link
-          href="https://fonts.googleapis.com/css?family=Fira+Code|Fira+Sans|Noto+Sans+SC&display=swap"
+          href="https://fonts.googleapis.com/css?family=Fira+Code|Fira+Sans&display=swap"
           rel="stylesheet"
         />
       </Helmet>
       <CanvasLayer location={location} />
-      <MainContainer>{children}</MainContainer>
+      {shouldRender ? <MainContainer>{children}</MainContainer> : null}
     </>
   )
 }
