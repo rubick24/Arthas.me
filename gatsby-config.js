e844416eac80fc2bc4c47372cc09beb4987690ae
rubick <spf@arthas.me>
2020-01-21 16:45:41 +0800

    helmet plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
