97e8eb0e64ad9ca0ca8280d88a82035f359a4249
rubick <spf@arthas.me>
2021-09-27 01:17:04 +0800

    style fix

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -15,58 +15,67 @@ export default function Post({ post, prev, next }) {
     return `${d.getFullYear()}.${d.getMonth() + 1}.${d.getDate()}`
   })()
 
-  return <>
-    <Head>
-      <meta name="image" content="/favicon.png" />
-      <title>{post.title} -Arthas.me</title>
-      {/* <meta name="description" content="random shits" /> */}
+  return (
+    <>
+      <Head>
+        <meta name="image" content="/favicon.png" />
+        <title>{post.title} -Arthas.me</title>
+        {/* <meta name="description" content="random shits" /> */}
 
-      <meta property="og:url" content={`https://arthas.me/posts/${post.slug}`} />
-      <meta property="og:type" content="article" />
-      <meta property="og:title" content={post.title} />
-      <meta property="og:image" content="/favicon.png" />
-      {/* <meta property="og:description" content="random shits" /> */}
+        <meta property="og:url" content={`https://arthas.me/posts/${post.slug}`} />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={post.title} />
+        <meta property="og:image" content="/favicon.png" />
+        {/* <meta property="og:description" content="random shits" /> */}
 
-      <meta name="twitter:title" content={post.title} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="/favicon.png" />
-      <meta name="twitter:creator" content="@deadalusmask" />
-      {/* <meta name="twitter:description" content="random shits" /> */}
+        <meta name="twitter:title" content={post.title} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="/favicon.png" />
+        <meta name="twitter:creator" content="@deadalusmask" />
+        {/* <meta name="twitter:description" content="random shits" /> */}
 
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossorigin="anonymous"/>
-    </Head>
-    {/* <h3>
-      <Link href="/"><a>Arthas.me</a></Link>
-      {' / '}
-      <Link href="/posts"><a>Posts</a></Link>
-      {' /'}
-    </h3> */}
-    <h1>{post.title}</h1>
-    <span>{timeStr}</span>
-    <div className="md-content" dangerouslySetInnerHTML={{__html: post.content}}></div>
-    <hr className="hr"/>
-    <div style={{display: 'flex', justifyContent: 'space-between', marginBottom: '24px'}}>
-      {prev ? <Link href={`/posts/${prev.slug}`}><a style={{marginRight: '24px'}}>{prev.title}</a></Link> : <a></a>}
-      {next ? <Link href={`/posts/${next.slug}`}><a>{next.title}</a></Link> : <a></a>}
-    </div>
-    <DiscussionEmbed
-      shortname='arthas-me'
-      config={{
-        url: `https://arthas.me/posts/${post.slug}`,
-        identifier: post.slug,
-        title: post.title,
-      }}
-    />
-  </>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
+          integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <h1>{post.title}</h1>
+      <span>{timeStr}</span>
+      <div className="md-content" dangerouslySetInnerHTML={{ __html: post.content }}></div>
+      <hr className="hr" />
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '24px' }}>
+        {prev ? (
+          <Link href={`/posts/${prev.slug}`}>
+            <a style={{ marginRight: '24px' }}>{prev.title}</a>
+          </Link>
+        ) : (
+          <a></a>
+        )}
+        {next ? (
+          <Link href={`/posts/${next.slug}`}>
+            <a>{next.title}</a>
+          </Link>
+        ) : (
+          <a></a>
+        )}
+      </div>
+
+      <DiscussionEmbed
+        shortname="arthas-me"
+        config={{
+          url: `https://arthas.me/posts/${post.slug}`,
+          identifier: post.slug,
+          title: post.title
+        }}
+      />
+    </>
+  )
 }
 
 export async function getStaticProps({ params }) {
-  const post = await getPostBySlug(params.slug, [
-    'title',
-    'date',
-    'slug',
-    'content',
-  ])
+  const post = await getPostBySlug(params.slug, ['title', 'date', 'slug', 'content'])
   if (!post) {
     return {}
   }
@@ -77,8 +86,8 @@ export async function getStaticProps({ params }) {
     props: {
       post,
       prev: index + 1 < posts.length ? posts[index + 1] : null,
-      next: index - 1 >= 0 ? posts[index - 1] : null,
-    },
+      next: index - 1 >= 0 ? posts[index - 1] : null
+    }
   }
 }
 
@@ -89,10 +98,10 @@ export async function getStaticPaths() {
     paths: posts.map(post => {
       return {
         params: {
-          slug: post.slug,
-        },
+          slug: post.slug
+        }
       }
     }),
-    fallback: false,
+    fallback: false
   }
 }
